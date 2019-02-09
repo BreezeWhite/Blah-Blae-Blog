6fa8b9a1db5d7a11b254de353265d348ae01dcd8
unknown <shizu.wu@blaysolutions.com>
2019-02-09 23:43:31 +0800

    intial upload

---
+++ firebase_init.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyArNQYfE-n9FtJKWUz0t62pxPR3zQ8Rx5g",
+  authDomain: "secret-voice-195911.firebaseapp.com",
+  databaseURL: "https://secret-voice-195911.firebaseio.com",
+  projectId: "secret-voice-195911",
+  storageBucket: "secret-voice-195911.appspot.com",
+  messagingSenderId: "771139968231"
+};
+firebase.initializeApp(config);
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
